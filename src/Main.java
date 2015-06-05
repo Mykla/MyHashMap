c8171e9892b0217682f0cb798f94013d8cc2e886
Mykla <mykla74@gmail.com>
2015-06-05 18:17:12 +0300

    Решить задачу подсчета повторяющихся элементов в массиве с помощью HashMap.

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,32 @@
 import java.util.*;
 /**
- * Решить задачу подсчета повторяющихся элементов в массиве с
- * помощью HashMap.
+ * Решить задачу подсчета повторяющихся элементов в массиве с помощью HashMap.
 */
 
 public class Main {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		Map<Integer, String> map = new HashMap<Integer, String>();
+
+		// исходный массив
 		map.put(1, "Человек1");
-		map.put(2, "Человек2");
-		map.put(3, "Человек3");
-		map.put(4, "Человек3");
-		map.put(7, "Человек3");
+		map.put(2, "Человек1");
+		map.put(3, "Человек1");
+		map.put(4, "Человек1");
+		map.put(5, "Человек3");
 		map.put(6, "Человек3");
 		map.put(7, "Человек3");
 
-		int[] list = new int[map.size()];
-		Set<Map.Entry<Integer, String>> valueMap = map.entrySet();
-
-		Set<Integer> set = map.keySet(); // создан сет ключей
-		int i = 0;
-		for (Integer key : set) {
-			int k = 0;
-			for (Map.Entry<Integer, String> entry : valueMap)// по всему массиву map
-			{
-				if (key == entry.getKey() && map.get(key).equals(entry.getValue()))
-				{
-					k++;
-				}
-			}
-			list[i] = k;
-			i++;
+		// создание коллекции уникальных значений
+		Set<String> uniqueSet = new HashSet<String>();
+		for (Integer key : map.keySet()) {
+			uniqueSet.add(map.get(key));
 		}
 
-		System.out.println(map.size());
+		// определение частоты повторов значений из коллекции uniqueSet
+		List<String> list = new ArrayList<String>(map.values());
+		for (String temp : uniqueSet) {
+			System.out.println(temp + ": " + Collections.frequency(list, temp));
+		}
 	}
 
-}
+	}
